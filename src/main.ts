f3c6ee209351d27352762f2e9ca2a89d37f03878
rafaelsene01 <rafaelssene@gmail.com>
2023-04-20 10:25:25 -0300

    fix: import

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ import './assets/main.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives
-})
+const vuetify = createVuetify()
 
 createApp(App).use(vuetify).mount('#app')
